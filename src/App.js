9f3e0fff0f979134cad79597443de56c4aa217b2
Muhamed Zuberovic <muhamed.zuberovic@intertek.com>
2018-11-20 14:57:59 +0100

    Added es lint rules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const App = () => {
-    return (
-        <div>
-            <p>React here!</p>
-        </div>
-    );
-};
+// const App = () => {
+//     return (
+//         <div>
+//             <p>React here!</p>
+//         </div>
+//     );
+// };
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>React here!</p>
+            </div>
+        );
+    }
+
+}
+
 export default App;
 ReactDOM.render(<App />, document.getElementById("app"));
