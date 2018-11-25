5dcfced54b7791b35faec4c7867326f9cf7ed6ba
Uraharadono <35668552+Uraharadono@users.noreply.github.com>
2018-11-25 22:26:41 +0100

    failed once again

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,36 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const flexbugs = require('postcss-flexbugs-fixes');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isDebug = process.env.NODE_ENV !== 'production';
+
+function configureCssLoader() {
+    const base = [
+        ...isDebug ? [{ loader: 'style-loader' }] : [],
+        { loader: 'css-loader', options: { url: true, importLoaders: 1 } },
+        {
+            loader: 'postcss-loader',
+            options: {
+                ident: 'postcss',
+                plugins: () => (isDebug
+                    ? [flexbugs, autoprefixer()]
+                    : [flexbugs, autoprefixer(), cssnano({ preset: 'default' })]
+                )
+            }
+        },
+        {
+            loader: 'sass-loader',
+            options: { precision: 9 }
+        }
+    ];
+
+    return !isDebug
+        ? ExtractTextPlugin.extract({ use: base, fallback: 'style-loader' })
+        : base;
+}
 
 module.exports = {
     module: {
@@ -20,18 +51,23 @@ module.exports = {
                     }
                 ]
             },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
+            // {
+            //     test: /\.css$/,
+            //     use: [MiniCssExtractPlugin.loader, "css-loader"]
+            // },
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         "style-loader", // creates style nodes from JS strings
+            //         "css-loader", // translates CSS into CommonJS
+            //         "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            //     ]
+            // }
             {
                 test: /\.scss$/,
-                use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader", // translates CSS into CommonJS
-                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
-                ]
-            }
+                exclude: /node_modules/,
+                use: configureCssLoader()
+            },
         ]
     },
     plugins: [
