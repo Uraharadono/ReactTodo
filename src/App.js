f6b80baeb976f240718d563294461b23dab7c25b
Muhamed Zuberovic <muhamed.zuberovic@intertek.com>
2018-11-27 13:54:06 +0100

    Finally fixed everything

--- src/App.js
+++ src/App.js
@@ -12,51 +12,45 @@ class App extends React.Component {
 
     render() {
         return (
-            <Router>
+            <Router >
                 <div>
                     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                        <a className="navbar-brand" href="/">Navbar</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarColor01">
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav mr-auto">
                                 <li className="nav-item active">
-                                    <Link to="/" className="nav-link">
-                                        Home
-                                 </Link>
-                                    {/* <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> */}
+                                    <Link to="/" className="nav-link">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    {/* <a className="nav-link" href="#">Features</a> */}
-                                    <Link to="todo-list" className="nav-link">
-                                        Todo list
-                                 </Link>
+                                    <Link to="todo-list" className="nav-link">Todo list</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link to="todo-list-2" className="nav-link">
-                                        Todo list 2
-                                 </Link>
-                                    {/* <a className="nav-link" href="#">Pricing</a> */}
+                                    <Link to="todo-list-2" className="nav-link">Todo list 2</Link>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a className="dropdown-item" href="/">Action</a>
+                                        <a className="dropdown-item" href="/">Another action</a>
+                                        <div className="dropdown-divider"></div>
+                                        <a className="dropdown-item" href="/">Something else here</a>
+                                    </div>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">About</a>
+                                    <a className="nav-link disabled" href="/">Disabled</a>
                                 </li>
                             </ul>
+                            <form className="form-inline my-2 my-lg-0">
+                                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
                         </div>
-
-
-                        {/* <header>
-                        <Link to="/">
-                            Home
-                        </Link>
-                        <Link to="/todo-list">
-                            Todo list
-                        </Link>
-                        <Link to="/todo-list-2">
-                            Todo list 2
-                        </Link>
-                    </header> */}
                     </nav>
 
                     <div>
